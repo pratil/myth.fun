c5bae0b91da7927880c4f33bd30cee1a3150321e
Pratil <itspratil@gmail.com>
2025-05-28 17:03:21 +0530

    update next js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/myth.fun",
-  assetPrefix: "/myth.fun/",
+  assetPrefix: "./",
+  trailingSlash: true,
 };
 
 export default nextConfig;
